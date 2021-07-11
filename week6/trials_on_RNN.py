2e11580064022b20bdbcc8f626f650af8d20d604
Paul Louis <iampaullouis@gmail.com>
2021-07-11 21:11:49 +0530

    Add some more comments

--- week6/trials_on_RNN.py
+++ week6/trials_on_RNN.py
@@ -1,6 +1,12 @@
 # Shakespeare trials using GRU and other RNNs
 # source is Section 2 in https://colab.research.google.com/github/lcharlin/80-629/blob/master/week6-RNNs%2BCNNs/RNNs_Answers.ipynb#scrollTo=49GUzizBbllV
 # With some comments from me to better understanding + moving the tensors and model to GPU
+# The above colab mentions this link: https://colab.research.google.com/drive/1jR_DGoVDcxZ104onxTk2C7YeV7vTt1DV#scrollTo=2bJvq3okegMw
+
+
+# Docs for GRU: https://pytorch.org/docs/master/generated/torch.nn.GRU.html#torch.nn.GRU
+# Help understand the dimensionality of input/output/hidden layers explained by referring the above link: https://stackoverflow.com/a/45023288
+
 import string
 
 import torch
@@ -16,6 +22,7 @@ device = "cuda"
 
 
 def random_chunk(chunk_len=200):
+    # randomly sample a chunk of 200 characters for training from input.txt
     start_index = random.randint(0, len(file) - chunk_len)
     end_index = start_index + chunk_len + 1
     return file[start_index:end_index]
@@ -30,9 +37,6 @@ def char_tensor(str, print_=False):
         if print_:
             print(f"Character: {str[c]} gets index {tensor[c][0]}")
     return tensor.to(device)
-
-
-char_tensor('Hello, how are you?', print_=True)
 
 
 def random_train_data():
@@ -53,6 +57,8 @@ class ShakespeareRNN(nn.Module):
         self.embeding_size = 6
 
         self.embedding = nn.Embedding(len(string.printable), self.embeding_size)
+        # GRU args are (6, 100, 2)
+        # args to GRU chosen below (num_features, hidden_size, number_of_layers to stack)
         self.rnn = nn.GRU(self.embeding_size, hidden_size, num_layers=n_layers)
         self.decoder = nn.Linear(hidden_size, output_size)
 
@@ -61,9 +67,13 @@ class ShakespeareRNN(nn.Module):
 
     def forward(self, input, hidden):
         x = self.embedding(input)
+        # The input dimensions are (seq_len, batch, input_size)
         # https://pytorch.org/docs/stable/tensor_view.html
+        # x is (1,6)
+        # we have to make x => (1, 1, 6)
         x, hidden = self.rnn(x.view(1, 1, x.size(-1)), hidden)
         output = self.decoder(x)
+        # output is (1, 1, 100) as self.output_size = 100
         # TODO: what is the use of variable `tag_scores`
         tag_scores = F.log_softmax(output[-1], dim=1)
         return output, hidden
@@ -112,6 +122,7 @@ def train_and_eval_shakespeare():
         input, target = random_train_data()
         chunk_len = len(input)
         hidden = model.init_hidden()
+        # the hidden dimensions are (num_layers, batch, hidden_size)
         # hidden.size = torch.Size([2, 1, 100])
 
         model.zero_grad()
@@ -130,5 +141,6 @@ def train_and_eval_shakespeare():
 
 
 if __name__ == '__main__':
+    char_tensor('ASCII', print_=True)
     model = train_and_eval_shakespeare()
     print(evaluate(model, 'Poll:', 100), '\n')
